f6f4434403adc6f12a27e103a0cb535f745ff15d
Njila Boris <njilaboris2@gmail.com>
2026-08-26 02:18:11 +0100

    Images

---
+++ data.ts
@@ -0,0 +1,666 @@
+export const deputiesData = [
+  {
+    "name": "AGHO Oliver BAMENJU",
+    "image": "AGHO-Oliver-BAMENJU.jpg"
+  },
+  {
+    "name": "AKA Martin TYOGA",
+    "image": "AKA-Martin-TYOGA.jpg"
+  },
+  {
+    "name": "ALI ADJIT",
+    "image": "ALI-ADJIT.jpg"
+  },
+  {
+    "name": "ALI MAMOUDA",
+    "image": "ALI-MAMOUDA.jpg"
+  },
+  {
+    "name": "ALI SALIHOU",
+    "image": "ALI-SALIHOU.jpg"
+  },
+  {
+    "name": "ALIYOUM FADIL",
+    "image": "ALIYOUM-FADIL.jpg"
+  },
+  {
+    "name": "AMINATOU ABBO",
+    "image": "AMINATOU-ABBO.jpg"
+  },
+  {
+    "name": "ANYERET CHARLES",
+    "image": "ANYERE-Charles-AKA.jpg"
+  },
+  {
+    "name": "ATEHWUNG FUJU Bernard",
+    "image": "ATEHWUNG-FUJU-Bernard.jpg"
+  },
+  {
+    "name": "AWUTAH Philip ATUBAH",
+    "image": "AWUTAH-Philip-ATUBAH.jpg"
+  },
+  {
+    "name": "BANMI Emmanuel DINGHA",
+    "image": "BANMI-Emmanuel-DINGHA.jpg"
+  },
+  {
+    "name": "BAORO Théophile",
+    "image": "BAORO-Theophile.jpg"
+  },
+  {
+    "name": "BARA Julien",
+    "image": "BARA-Julien.jpg"
+  },
+  {
+    "name": "BELLO LIMANE",
+    "image": "BELLO-LIANE.jpg"
+  },
+  {
+    "name": "BENAE Serge Gabriel",
+    "image": "BENAE-Serge-Gabriel.jpg"
+  },
+  {
+    "name": "BIBA François",
+    "image": "BIBA-Francois-Prostin.jpg"
+  },
+  {
+    "name": "BILOA TSILLA Zitha",
+    "image": "BILOA-TSILLA-ZITHA-Marie-Isabelle.jpg"
+  },
+  {
+    "name": "BINDOUA Mathurin Germain",
+    "image": "BINDOUA-Mathurin-Germain.jpg"
+  },
+  {
+    "name": "BITHBINI Rosaline",
+    "image": "BITHBINI-Rosaline.jpg"
+  },
+  {
+    "name": "BOUBA MOUSSA",
+    "image": "BOUBA-MOUSSA.jpg"
+  },
+  {
+    "name": "CAVAYE YEGUIE Djibril",
+    "image": "CAVAYE-YEGUIE-DJIBRIL.jpg"
+  },
+  {
+    "name": "CECILIA DIONE EPIE NJUMBE",
+    "image": "Cecilia-DIONNE-EPIE-NJUMBE.jpg"
+  },
+  {
+    "name": "DAGUE AÏCHA Blanche",
+    "image": "DAGUE-AICHA-Blanche-Jacqueline.jpg"
+  },
+  {
+    "name": "DAMDAM Marie",
+    "image": "DAMADAM-Marie.jpg"
+  },
+  {
+    "name": "DIMBELE BOUI",
+    "image": "DIMBELE-BOUI.jpg"
+  },
+  {
+    "name": "DISSAKE née EKOKA Marguerite Hélène",
+    "image": "DISSAKE-nee-EKOKA-Marguerite-Helene.jpg"
+  },
+  {
+    "name": "DJABOU Marie Solange",
+    "image": "DJABOU-Marie-Solange.jpg"
+  },
+  {
+    "name": "DJEUMENI BENILDE",
+    "image": "DJEMENI-BENILDE.jpg"
+  },
+  {
+    "name": "DJIBRILLA KAOU",
+    "image": "DJIBRILA-KAO.jpg"
+  },
+  {
+    "name": "DJOMGOUE PAUL ERIC",
+    "image": "DJOMGOUE-PAUL-ERIC.jpg"
+  },
+  {
+    "name": "DONALD MALOMBA ESEMBE",
+    "image": "DONALD-MALOMBA-ESEMBE.jpg"
+  },
+  {
+    "name": "DONGHO Clément",
+    "image": "DONGO-Clément.jpg"
+  },
+  {
+    "name": "DOOH COLLINS Albert KOUOH",
+    "image": "DOOH.jpg"
+  },
+  {
+    "name": "DOUVAOUISSA AÏSSA HAMADI",
+    "image": "DOUVAOUISSA-AISSA-HAMADI.jpg"
+  },
+  {
+    "name": "EBANGHA Epse AGBORNTUI Johanna",
+    "image": "EBANGHA-épse-AGBORNTUI-Johanna.jpg"
+  },
+  {
+    "name": "EKOLLE Peters",
+    "image": "EKOLLE-Peters.jpg"
+  },
+  {
+    "name": "Elise NDONGO MOUTOME Epse POKOSSY NDOUMBE",
+    "image": "NDONGO-MOUTOME-Elise-epse-POKOSSI-DOUMBE.jpg"
+  },
+  {
+    "name": "EMABOT Brigitte",
+    "image": "EMABOT-Brigitte.jpg"
+  },
+  {
+    "name": "EMAH ETOUNDI VINCENT DE PAUL",
+    "image": "EMAH-ETOUNDI-VINCENT-DE-PAUL.jpg"
+  },
+  {
+    "name": "EPOUBE née EYOUM MINONO Lydienne",
+    "image": "EYOUM-MINONO-épse-EPOUBE-Lydienne.jpg"
+  },
+  {
+    "name": "ESSAMA NKOUDOU Christophe",
+    "image": "ESSAMA-NKOUDOU-C.jpg"
+  },
+  {
+    "name": "ESSOMBA BENGONO Engelbert Alain",
+    "image": "ESSOMBA-BENGONO-Engelbert-Alain.jpg"
+  },
+  {
+    "name": "ESSONO Francis Lin Mathieu",
+    "image": "ESSONO-Francis-Lin-Mathieu.jpg"
+  },
+  {
+    "name": "ETOMBI IKOME Gladys",
+    "image": "ETOMBI-BOLEVIE-Gladys.jpg"
+  },
+  {
+    "name": "EVINA OVAMBE Bernadette Epse ABOUI",
+    "image": "EVINA-OVAMBE-Bernadette-Epse-ABOUI.jpg"
+  },
+  {
+    "name": "FADIMATOU SAMBO",
+    "image": "FADIMATOU-SAMBO.jpg"
+  },
+  {
+    "name": "FANDJA Gabriel",
+    "image": "FANDJA-Gabriel.jpg"
+  },
+  {
+    "name": "FEUTHEU Jean Claude",
+    "image": "FEUTHEU-Jean-Claude.jpg"
+  },
+  {
+    "name": "FINDI STANLEY MOKONDO",
+    "image": "FINDI-Stanley-MOKONDO.jpg"
+  },
+  {
+    "name": "FONGE FONGOUCK Julius",
+    "image": "FONGE-Julius-FONGOUCK.jpg"
+  },
+  {
+    "name": "GHIMBOP Joséphine",
+    "image": "GHIMBOP-SIMO-NDIFFO-Josephine.jpg"
+  },
+  {
+    "name": "GONONDO Jean",
+    "image": "GONONDO-Jean.jpg"
+  },
+  {
+    "name": "GUISWE-BADOMA",
+    "image": "GUISWE-BADOMA.jpg"
+  },
+  {
+    "name": "HALIA MOUSSA MOUFTA",
+    "image": "HALIA-MOUSSA-MOUFTA.jpg"
+  },
+  {
+    "name": "HAMADOU SALI",
+    "image": "HAMADOU-SALI.jpg"
+  },
+  {
+    "name": "HAMAN TCHIOUTO",
+    "image": "HAMAN-TCHIOUTO.jpg"
+  },
+  {
+    "name": "HAROUNA ABDOULAYE",
+    "image": "HAROUNA-ABDOULAYE.jpg"
+  },
+  {
+    "name": "IBRAHIMA BOBBO",
+    "image": "IBRAHIMA-BOBBO-BELLO.jpg"
+  },
+  {
+    "name": "INJOH FO'O NGANG Prudencia",
+    "image": "INJOH-FOO-NGANG-Prudencia.jpg"
+  },
+  {
+    "name": "JUIMO SIEWE MONTHE Claude",
+    "image": "JUIMO-SIEWE-MONTHE-Claude.jpg"
+  },
+  {
+    "name": "KAMSSOULOUM ABBA KABIR",
+    "image": "KAMSSOULOUM-ABBA-KABIR.jpg"
+  },
+  {
+    "name": "KENFACK Sonna NGNEGUIM Anastasie",
+    "image": "KENFACK-SONNA-NGUEGUIM-Anastasie.jpg"
+  },
+  {
+    "name": "KOAH SONGO Gabriel",
+    "image": "KOA-SONGO.jpg"
+  },
+  {
+    "name": "KOMBO GBERI",
+    "image": "KOMBO-GBERI.jpg"
+  },
+  {
+    "name": "KOUINCHE Albert",
+    "image": "KOUINCHE-Albert.jpg"
+  },
+  {
+    "name": "KUM JOHN NJI",
+    "image": "KUM-John-NJI.jpg"
+  },
+  {
+    "name": "KUMASE Simon",
+    "image": "KUMASE-Simon-POWOH.jpg"
+  },
+  {
+    "name": "KWARMBA Solange",
+    "image": "KWARMBA-Solange.jpg"
+  },
+  {
+    "name": "KWEI Andrew MNGO",
+    "image": "KWEI-Andrew-MNGO.jpg"
+  },
+  {
+    "name": "KWEMO Pierre",
+    "image": "KWEMO-Pierre.jpg"
+  },
+  {
+    "name": "LAWSON TABOT BAKIA",
+    "image": "LAWSON-TABOT-BAKIA.jpg"
+  },
+  {
+    "name": "LIBII LI NGUE NGUE Cabral",
+    "image": "LIBI-LI-NGUE-CABRAL.jpg"
+  },
+  {
+    "name": "LUKONG Fostine FOMUY",
+    "image": "LUKONG-Fostine-FOMUY.jpg"
+  },
+  {
+    "name": "MAGOUO Salomé",
+    "image": "MAGOUO-Salome.jpg"
+  },
+  {
+    "name": "MAN Jacqueline Christiane",
+    "image": "MAN-Jacqueline-Christiane.jpg"
+  },
+  {
+    "name": "MANAMOUROU I SILIKAM",
+    "image": "MANAMOUROU-I-SILIKAM.jpg"
+  },
+  {
+    "name": "MANDIO William Peter",
+    "image": "MANDIO-William-Peter.jpg"
+  },
+  {
+    "name": "MANFOUO David",
+    "image": "MANFOUO-David.jpg"
+  },
+  {
+    "name": "MARGAZA ALAOU ABAL AGGI",
+    "image": "MARGAZA-ALAOU-ABAL-MAGGI.jpg"
+  },
+  {
+    "name": "Mariam GONI",
+    "image": "Mariam-GONI.jpg"
+  },
+  {
+    "name": "MBAKAM CHOUGA Guillaume",
+    "image": "MBAKAM-CHOUGA-Guillaume.jpg"
+  },
+  {
+    "name": "MBALA NGOBO",
+    "image": "MBALA-NGOBO-Cathérine-épse-MFOULA.jpg"
+  },
+  {
+    "name": "MBAPTE Jean Baptiste",
+    "image": "MBAPTE-Jean-Baptiste.jpg"
+  },
+  {
+    "name": "MBARGA ASSEMBE Luc",
+    "image": "MBARGA-ASSEMBE-Luc-Roger.jpg"
+  },
+  {
+    "name": "MBE ASSAE MENDOMO Théodore Alexandre",
+    "image": "MBE-ASSAE-MENDOMO-Théodore.jpg"
+  },
+  {
+    "name": "MBEDE Pierre Petrus",
+    "image": "MBEDE-Pierre-Petrus.jpg"
+  },
+  {
+    "name": "MBEYO'O Josué",
+    "image": "MBEYO-O-Josue.jpg"
+  },
+  {
+    "name": "MBONGYOR NAOMI NGANDO",
+    "image": "MBONGYOR-Naomi-NGANDO.jpg"
+  },
+  {
+    "name": "MBOUANGOUERE Rainatou Epse MONGWAT",
+    "image": "MBOUANGOUERE-RAINATOU-Epse-MONGWAT.jpg"
+  },
+  {
+    "name": "MBUWIR Tobias",
+    "image": "MBUWIR-Tobias-BANBONG.jpg"
+  },
+  {
+    "name": "MEKONGO Hélène Épse ATANGANA",
+    "image": "MEKONGO-Hélène-épse-ATANGANA.jpg"
+  },
+  {
+    "name": "MELINGUI Roger",
+    "image": "MELINGUI-Roger.jpg"
+  },
+  {
+    "name": "MEMOUNA MAHAMAT",
+    "image": "MEMOUNA-MAHAMAT.jpg"
+  },
+  {
+    "name": "MENANA Née NANGA Marthe Georgette",
+    "image": "NANGA-epse-MENANA.jpg"
+  },
+  {
+    "name": "MENGUE MEZUI Germain Durand",
+    "image": "MENGUE-MEZUI-Germain-Durand.jpg"
+  },
+  {
+    "name": "MOHAMADOU MAHDI",
+    "image": "MOHAMADOU-MAHDI.jpg"
+  },
+  {
+    "name": "MOLUH NOURANE HASSANA",
+    "image": "NOURANE-MOLUH-épse-FOTSING-HASSANA.jpg"
+  },
+  {
+    "name": "MONJOWA LIFAKA Emilia",
+    "image": "MONJOWA-LIFAKA-E.jpg"
+  },
+  {
+    "name": "MOTH Samuel Dieudonné",
+    "image": "MOTH-Samuel-Dieudonné.jpg"
+  },
+  {
+    "name": "MOUMINI OUMAROU",
+    "image": "MOUMIMI-OUMAROU.jpg"
+  },
+  {
+    "name": "MOUSSOUS LISSISSOU",
+    "image": "MOSSOOUS-LISSISSOU.jpg"
+  },
+  {
+    "name": "MOUTYMBO Rosette Julienne épouse AYAYI",
+    "image": "MOUTYMBO-epse-AYAYI-Rosette-Julienne.jpg"
+  },
+  {
+    "name": "MPON François Xavier",
+    "image": "MPON-Francois-Xavier.jpg"
+  },
+  {
+    "name": "MUYALI BOYA Mary Epsé MEBOKO",
+    "image": "Mary-MUYALI-BOYA-epse-MEBOKA.jpg"
+  },
+  {
+    "name": "MVONDO ASSAM Bonaventure",
+    "image": "MVONDO-ASSAM-Bonaventure.jpg"
+  },
+  {
+    "name": "N'NOLO Marie Suzanne Épse ONOBIONO",
+    "image": "NNOLO-epse-ONOBIONO-Marie-Suzane.jpg"
+  },
+  {
+    "name": "NAFISSATOU ALIM",
+    "image": "NAFISSATOU-ALIM.jpg"
+  },
+  {
+    "name": "NANGA MEFANT Berthe Épse OWONO",
+    "image": "NANGA-MEFANT-Berthe-épse-OWONO.jpg"
+  },
+  {
+    "name": "NDAM",
+    "image": "NDAM.jpg"
+  },
+  {
+    "name": "NDJIP Bienvenu",
+    "image": "NDJIP-Bienvenu.jpg"
+  },
+  {
+    "name": "NDONGO ESSOMBA Jean Bernard",
+    "image": "NDONGO-ESSOMBA-J-B.jpg"
+  },
+  {
+    "name": "NDONGO ETEME Edgard",
+    "image": "NDONGO-ETEME-Edgard.jpg"
+  },
+  {
+    "name": "NDOUMOU née BEKONO Pauline",
+    "image": "BEKONO-EBAH-Pauline-epse-NDOUMOU.jpg"
+  },
+  {
+    "name": "NDZIE Frank Eric",
+    "image": "NDZIE-Francik-Eric.jpg"
+  },
+  {
+    "name": "NGABA ZOGO Salomé",
+    "image": "NGABA-ZOGO-Salome.jpg"
+  },
+  {
+    "name": "NGAH Estalla Tala",
+    "image": "NGAH-Estella-Shala.jpg"
+  },
+  {
+    "name": "NGAHANE",
+    "image": "NGAHANE.jpg"
+  },
+  {
+    "name": "NGALA GERARD NDOMBANG",
+    "image": "NGALA-Gerard-NDOMBANG.jpg"
+  },
+  {
+    "name": "NGANTCHA Louis Henri",
+    "image": "NGANTCHA-Louis-Henri.jpg"
+  },
+  {
+    "name": "NGBANBAYE Antoinette",
+    "image": "NGBANBAYE-Antointte.jpg"
+  },
+  {
+    "name": "NGNEGUE Simon",
+    "image": "NGNEGUE-Simon.jpg"
+  },
+  {
+    "name": "NGO ISSI Rolande",
+    "image": "NGO-ISSI-Rolande-Adèle.jpg"
+  },
+  {
+    "name": "NGO YETNA Marinette Epse MBELEG",
+    "image": "NGO-YETNA-Rosette-épse-MBELEG.jpg"
+  },
+  {
+    "name": "NGOKO NAMBE Marie Louise",
+    "image": "NGOKO-MAMBE-Marie-Louise-Eps-TCHOUANTE.jpg"
+  },
+  {
+    "name": "NGUENKAM Epse TCHOUAGA Marie Louise",
+    "image": "NGUEKAM-Epse-TCHOUAGA-Marie-Louise.jpg"
+  },
+  {
+    "name": "NIKINA Pierre",
+    "image": "NIKINA-Pierre.jpg"
+  },
+  {
+    "name": "NINTCHEU Jean Michel",
+    "image": "NINTCHEU-Jean-Michel.jpg"
+  },
+  {
+    "name": "NJINGUM MUSA MBUTOH",
+    "image": "NJINGUM-MUSA.jpg"
+  },
+  {
+    "name": "NJONG EVARISTUS NDIM",
+    "image": "NJONG-Evaristus-NDIM.jpg"
+  },
+  {
+    "name": "NJUME Peter AMBANG",
+    "image": "NJUME-Peter-AMBANG.jpg"
+  },
+  {
+    "name": "NKODO DANG Roger",
+    "image": "NKODO-DANG-ROGER.jpg"
+  },
+  {
+    "name": "OSIH JOSHUA",
+    "image": "Joshua-NAMBANGIOSIH.jpg"
+  },
+  {
+    "name": "OUMOUL KOULCHOUMI Épse MOHAMADOU AHIDJO",
+    "image": "OUMOUL-KOULTCHOUMI-Epse-AHIDJO-MOHAMADOU.jpg"
+  },
+  {
+    "name": "OUSMANOU AMAN SA'ALY",
+    "image": "OUSMANOU-AMAN-SA-ALY.jpg"
+  },
+  {
+    "name": "OUSMANOU DAWAÏ",
+    "image": "OUSMANOU-DAWAI.jpg"
+  },
+  {
+    "name": "PONGOH Emmanuel",
+    "image": "PONGOH-Emmanuel.jpg"
+  },
+  {
+    "name": "PRINCE MIKODY Ange Gilbert",
+    "image": "PRINCE-MIKODY-Ange-Gilbert.jpg"
+  },
+  {
+    "name": "SALAMANA AMADOU ALI II",
+    "image": "SALAMANA-AMADOU-ALI-II.jpg"
+  },
+  {
+    "name": "SALOMON DOUVOGO",
+    "image": "SALOMON-DOUVOGO.jpg"
+  },
+  {
+    "name": "SAMBO Galdima",
+    "image": "SAMBO-GALDIMA.jpg"
+  },
+  {
+    "name": "SAROU Bernadette",
+    "image": "SARAOU-Bernadette.jpg"
+  },
+  {
+    "name": "SAYA KAIGAMA",
+    "image": "SAYA-KAIGAMA.jpg"
+  },
+  {
+    "name": "SIME Pierre",
+    "image": "SIME-Pierre.jpg"
+  },
+  {
+    "name": "SOHAING Colette",
+    "image": "KAM-Colette-épse-SOHAING.jpg"
+  },
+  {
+    "name": "SOPPO TOUTE MARLYSE",
+    "image": "SOPPO-TOUTE-MARLYSE.jpg"
+  },
+  {
+    "name": "TABOULI Celestin",
+    "image": "TABOULI-Celestin.jpg"
+  },
+  {
+    "name": "TAK Bienvenu",
+    "image": "TAK-Bienvenu.jpg"
+  },
+  {
+    "name": "TANEFO Jean Marie",
+    "image": "TANEFO-Jean-Marie.jpg"
+  },
+  {
+    "name": "TASSI Evelyne Chantale Epse AYISSI",
+    "image": "TASSI-Eveline-Chantal-Denise-epse-AYISSI.jpg"
+  },
+  {
+    "name": "TCHAGNA Jaqueline Rêve Angeline",
+    "image": "TCHAGNA-Jaceline-Reve-Angèline.jpg"
+  },
+  {
+    "name": "TCHENDE MAHAMA",
+    "image": "TCHENDE-MAHAMA.jpg"
+  },
+  {
+    "name": "TEKOURA Paul Blaise",
+    "image": "TEKOURA-Paul-Blaise.jpg"
+  },
+  {
+    "name": "TEKU TANYI TEKU",
+    "image": "TEKU-TANYI-TEKU.jpg"
+  },
+  {
+    "name": "TIKOBAU Pierre Marie",
+    "image": "TIKOBAU-Pierre-Marie.jpg"
+  },
+  {
+    "name": "TOUKAM Angèle Tela",
+    "image": "TOUKAM-Angèle.jpg"
+  },
+  {
+    "name": "TSINGANG Flobert",
+    "image": "TSINGANG-Flobert.jpg"
+  },
+  {
+    "name": "WA Mathurin",
+    "image": "WA-Mathurin-Martial.jpg"
+  },
+  {
+    "name": "WAINACHI Honorine",
+    "image": "WAINACHI-NENGTOH-Honourine.jpg"
+  },
+  {
+    "name": "WALLANG Richard EBUA",
+    "image": "WALLANG-Richard-EBUA.jpg"
+  },
+  {
+    "name": "YAKOUBA YAYA",
+    "image": "YAKOUBA-YAYA.jpg"
+  },
+  {
+    "name": "YAOUBA ALHADJI",
+    "image": "YAOUBA-ALHADJI.jpg"
+  },
+  {
+    "name": "YAYA DOUMBA Marius",
+    "image": "YAYA-DOUMBA-Marius.jpg"
+  },
+  {
+    "name": "YEBA Judith SAMA Epse MOUOKUIO",
+    "image": "YEBA-Judih-SAMA.jpg"
+  },
+  {
+    "name": "YOUMO KOUPIT ADAMOU",
+    "image": "YOUMO-NKOUPIT-ADAMOU.jpg"
+  },
+  {
+    "name": "ZAM Jean Jacques Noel",
+    "image": "ZAM-Jean-Jacques-Noel.jpg"
+  },
+  {
+    "name": "ZONDOL HERSSESSE",
+    "image": "ZONDOL-HERSSESSE.jpg"
+  }
+]
