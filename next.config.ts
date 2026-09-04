5a463b4b651996849c200a9f8988660fcfe6ca54
Njila Boris <njilaboris2@gmail.com>
2026-09-04 22:43:06 +0100

    image

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'www.assnat.cm',
-        pathname: '',
+         pathname: '/**',
       },
     ],
   },  /* config options here */
